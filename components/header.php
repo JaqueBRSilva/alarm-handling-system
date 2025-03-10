df7f75e184955b0d5d799732a916ba9301211ac7
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-03-10 15:44:00 -0300

    refactor: rem. links p/ 'dashboard'

--- components/header.php
+++ components/header.php
@@ -12,7 +12,7 @@
 <div class="container">
   <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
     <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      <i class="bi bi-shield-check me-2" style="font-size: 2rem; color: " ></i>
       <span class="fs-4">Gerenciamento de Alarmes</span>
     </a>
 
@@ -20,11 +20,6 @@
       <li class="nav-item">
         <a href="index.php" class="nav-link <?php echo isPageActive('index.php') ?>" aria-current="page">
           Home
-        </a>
-      </li>
-      <li class="nav-item">
-        <a href="dashboard.php" class="nav-link <?php echo isPageActive('dashboard.php') ?>" >
-          Dashboard
         </a>
       </li>
       <li class="nav-item">
