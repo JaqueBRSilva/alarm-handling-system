803d0ed47f9da01b4c660d26fcb6fad992080ed3
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-03-10 11:02:00 -0300

    feat: add 'footer'

---
+++ components/footer.php
@@ -0,0 +1,14 @@
+<div class="container flex mt-4">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="index.php" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="alarms.php" class="nav-link px-2 text-body-secondary">Alarmes</a></li>
+            <li class="nav-item"><a href="equipaments.php" class="nav-link px-2 text-body-secondary">Equipamentos</a></li>
+        </ul>
+
+        <p class="text-center text-body-secondary">&copy; 2025 JaqueBRSilva</p>
+    </footer>
+</div>
+
+</body>
+</html>
