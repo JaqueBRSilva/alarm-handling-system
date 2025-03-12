6e959b3327c005520a18f326201839fbe1a1b17b
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-03-12 16:30:34 -0300

    styles: alterado nome de classe do footer

--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,9 @@
-<?php include '../components/head.php' ?>
-
-<div class="footer-container">
-    <footer class="py-3 mt-4" style="">
+<div class="footer-area">
+    <footer class="py-3 mt-5">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="index.php" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li class="nav-item"><a href="alarms.php" class="nav-link px-2 text-body-secondary">Alarmes</a></li>
-            <li class="nav-item"><a href="equipaments.php" class="nav-link px-2 text-body-secondary">Equipamentos</a></li>
+            <li class="nav-item"><a href="../public/index.php" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="../public/alarms.php" class="nav-link px-2 text-body-secondary">Alarmes</a></li>
+            <li class="nav-item"><a href="../public/equipaments.php" class="nav-link px-2 text-body-secondary">Equipamentos</a></li>
         </ul>
 
         <p class="text-center text-body-secondary">&copy; 2025 JaqueBRSilva</p>
