ce2496e26c73f6b0879eb3031481f7a4187f3df1
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-03-12 17:56:04 -0300

    refactor: rem. links de nav do 'footer'

--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,5 @@
 <div class="footer-area">
-    <footer class="py-3 mt-5">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="../public/index.php" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li class="nav-item"><a href="../public/alarms.php" class="nav-link px-2 text-body-secondary">Alarmes</a></li>
-            <li class="nav-item"><a href="../public/equipaments.php" class="nav-link px-2 text-body-secondary">Equipamentos</a></li>
-        </ul>
-
+    <footer class="py-3 justify-content-center border-top">
         <p class="text-center text-body-secondary">&copy; 2025 JaqueBRSilva</p>
     </footer>
 </div>
